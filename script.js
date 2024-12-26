3f298d0f4c9b1acb8841edbc6e8f1d2799cc9f90
Satender yadav <146433987+Technology-maker@users.noreply.github.com>
2024-12-26 15:39:36 +0530

    update files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive Navbar Toggle
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
